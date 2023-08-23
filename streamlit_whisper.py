ada02c2822106c673779572e108c888778e1e6cb
Ryan Moore <mooreaz92@gmail.com>
2023-08-22 23:55:08 -0500

    EOD Commit; estab app skel and transcrip module

--- streamlit_whisper.py
+++ streamlit_whisper.py
@@ -1,69 +1,68 @@
 import streamlit as st
 import whisper
-import time 
 
-# Create a Streamlit sidebar for navigation
+### Formatting
+
+# Create title and sidebar for navigation
+st.title("AI Auto Summarization")
 st.sidebar.title("Navigation")
 selected_section = st.sidebar.radio("Workflow", ["Transcription", "Summarize"])
 
-# Main content area
-st.title("AI Auto Summarization")
+### Variables and Model Loading
 
-# Define variables for transcription and summarization
+# Transcription model and variables
 transcription_model = whisper.load_model("base")
+
 transcription_text = ""
 transcription_in_progress = False
-summarization_in_progress = False
 transcript_uploaded = False
 
-# Function to simulate transcription
-def simulate_transcription(audio_file):
-    global transcription_text, transcription_in_progress
-    transcription_in_progress = True
-    st.success("Transcribing Audio")
-    
-    # Simulate transcription, replace this with actual code
-    for i in range(1, 101):
-        time.sleep(0.1)  # Simulate some processing time
-        transcription_text += f"Transcribed text line {i}\n"
-        st.experimental_set_query_params(transcription_progress=i)  # Update progress in URL
-    transcription_in_progress = False
-    st.success("Transcription Complete")
+# Summarization model and variables
 
-# Function to simulate summarization
-def simulate_summarization(transcription_text):
+summarization_in_progress = False
+
+### Function Definitions
+
+# Transcription function 
+def transcribe_audio(audio_file_path):
+    global transcription_in_progress, transcript_uploaded
+    transcription_in_progress = True
+    transcription = transcription_model.transcribe(audio_file_path)
+    transcription_in_progress = False
+    transcript_uploaded = True
+    return transcription
+
+# Summarization function
+def summarize_transcription(transcription_text):
     global summarization_in_progress
     summarization_in_progress = True
-    st.success("Summarizing Transcript")
-    
-    # Simulate summarization, replace this with actual code
-    # You can use a language model for summarization here
     summarized_text = "This is a summary of the transcription.\nIt can be quite long."
     summarization_in_progress = False
-    st.success("Summarization Complete")
-    
     return summarized_text
 
-# Transcription section
+### Transcription Section
+
 if selected_section == "Transcription":
     st.subheader("Transcription")
     
+    # Button to upload audio file
     audio_file = st.file_uploader("Upload audio file for transcription", type=["mp3", "wav", "m4a"])
-    
+
+    # Button to transcribe audio file
     if audio_file is not None and not transcription_in_progress:
-        transcript_uploaded = True
         if st.button("Transcribe Audio"):
-            simulate_transcription(audio_file)
+            cached_transcript = transcribe_audio(audio_file.name)
+            st.success("Transcription Complete")
+            transcript_uploaded = True
 
-    # Display transcription progress
-    if transcription_in_progress:
-        progress = st.experimental_get_query_params().get('transcription_progress', 0)
-        st.progress(progress)
+    # Display a truncated version of the transcript only when the transcribe button has been pressed
 
-    # Display a truncated version of the transcript when transcription is complete
-    if not transcription_in_progress and transcript_uploaded:
-        st.subheader("Transcription Result")
-        st.write(transcription_text[:500])  # Display the first 500 characters, adjust as needed
+    if transcript_uploaded:
+        st.subheader("Transcription Sample")
+        st.write(cached_transcript["text"][:1000])
+
+### Summarization Section
+
 
 # Summarize section
 if selected_section == "Summarize":
