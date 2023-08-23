d106b1821d5bfbc2e07be68849c01f844aede3e5
Ryan Moore <mooreaz92@gmail.com>
2023-08-22 21:41:40 -0500

    v2.0 of app now that ffmpeg issue is resolved

---
+++ streamlit_whisper.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import whisper
+import time 
+
+# Create a Streamlit sidebar for navigation
+st.sidebar.title("Navigation")
+selected_section = st.sidebar.radio("Workflow", ["Transcription", "Summarize"])
+
+# Main content area
+st.title("AI Auto Summarization")
+
+# Define variables for transcription and summarization
+transcription_model = whisper.load_model("base")
+transcription_text = ""
+transcription_in_progress = False
+summarization_in_progress = False
+transcript_uploaded = False
+
+# Function to simulate transcription
+def simulate_transcription(audio_file):
+    global transcription_text, transcription_in_progress
+    transcription_in_progress = True
+    st.success("Transcribing Audio")
+    
+    # Simulate transcription, replace this with actual code
+    for i in range(1, 101):
+        time.sleep(0.1)  # Simulate some processing time
+        transcription_text += f"Transcribed text line {i}\n"
+        st.experimental_set_query_params(transcription_progress=i)  # Update progress in URL
+    transcription_in_progress = False
+    st.success("Transcription Complete")
+
+# Function to simulate summarization
+def simulate_summarization(transcription_text):
+    global summarization_in_progress
+    summarization_in_progress = True
+    st.success("Summarizing Transcript")
+    
+    # Simulate summarization, replace this with actual code
+    # You can use a language model for summarization here
+    summarized_text = "This is a summary of the transcription.\nIt can be quite long."
+    summarization_in_progress = False
+    st.success("Summarization Complete")
+    
+    return summarized_text
+
+# Transcription section
+if selected_section == "Transcription":
+    st.subheader("Transcription")
+    
+    audio_file = st.file_uploader("Upload audio file for transcription", type=["mp3", "wav", "m4a"])
+    
+    if audio_file is not None and not transcription_in_progress:
+        transcript_uploaded = True
+        if st.button("Transcribe Audio"):
+            simulate_transcription(audio_file)
+
+    # Display transcription progress
+    if transcription_in_progress:
+        progress = st.experimental_get_query_params().get('transcription_progress', 0)
+        st.progress(progress)
+
+    # Display a truncated version of the transcript when transcription is complete
+    if not transcription_in_progress and transcript_uploaded:
+        st.subheader("Transcription Result")
+        st.write(transcription_text[:500])  # Display the first 500 characters, adjust as needed
+
+# Summarize section
+if selected_section == "Summarize":
+    st.subheader("Summarize")
+    if st.button("Summarize Transcription") and not summarization_in_progress:
+        summarization_text = simulate_summarization(transcription_text)
+        st.subheader("Summarized Text")
+        st.write(summarization_text)
+
