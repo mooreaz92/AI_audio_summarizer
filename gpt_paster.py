54a04f85e750acad57c501d4254f81a3c8d8f411
Ryan Moore <mooreaz92@gmail.com>
2023-08-23 22:19:20 -0500

    EOD commit; first code blocks of summarizer

--- gpt_paster.py
+++ gpt_paster.py
@@ -1,30 +1,53 @@
 import streamlit as st
-from streamlit.server.server import Server
+from stqdm import stqdm
+import time  # Replace this with your actual audio transcription library
 
-def get_session_id():
-    # This function generates a unique session ID for the current session.
-    session_id = id(Server.get_current()._session_info.session)
-    return session_id
+# Define a function to simulate audio transcription with progress tracking
+def transcribe_audio_with_progress(pseudo_file_path):
+    total_steps = 100  # You should adjust this based on your actual progress tracking
+    for step in range(total_steps):
+        # Simulate a step in the transcription process
+        # Replace this with your actual audio transcription code
+        time.sleep(0.1)  # Simulate a small delay for demonstration
 
-# Function to initialize SessionState
-def init_session_state():
-    session_id = get_session_id()
-    if not hasattr(st, 'session_state'):
-        st.session_state[session_id] = {}
+        # Update the progress bar
+        stqdm(status=f"Transcribing audio: {step+1}/{total_steps}", key="transcription_progress")
+    
+    return {"text": "This is the transcribed text."}
 
-init_session_state()
+# Initialize Streamlit
+st.title("Audio Transcription App")
 
-# File Upload
-uploaded_file = st.file_uploader("Upload a file", type=["txt", "pdf", "docx"])
+# Define your Streamlit session state variables
+if "transcript_text" not in st.session_state:
+    st.session_state.transcript_text = None
+if "transcription_in_progress" not in st.session_state:
+    st.session_state.transcription_in_progress = False
 
-# Store the uploaded file in the session_state
-if uploaded_file is not None:
-    session_id = get_session_id()
-    st.session_state[session_id]['file'] = uploaded_file
-    st.write("File uploaded successfully!")
+# Upload an audio file
+audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
 
-# Access the uploaded file in your transcriber function
-session_id = get_session_id()
-if 'file' in st.session_state[session_id]:
-    uploaded_file = st.session_state[session_id]['file']
-    # Now you can pass `uploaded_file` to your transcriber function
+# Check if the "Transcribe Audio" button is clicked
+if audio_file is not None and not st.session_state.transcription_in_progress:
+    if st.button("Transcribe Audio"):
+        # Set transcription in progress to True
+        st.session_state.transcription_in_progress = True
+
+        # Use stqdm to create a progress bar
+        with stqdm(total=100, desc="Transcribing audio", key="transcription_progress") as progress_bar:
+            # Call your transcription function with progress tracking
+            transcript_cached = transcribe_audio_with_progress(audio_file)
+
+            # Store the transcript in session state
+            st.session_state.transcript_text = transcript_cached["text"]
+
+            # Indicate that the transcription is complete
+            st.success("Transcription Complete")
+
+        # Set transcription in progress back to False
+        st.session_state.transcription_in_progress = False
+
+# Display the transcript
+if st.session_state.transcript_text:
+    st.subheader("Transcript:")
+    st.write(st.session_state.transcript_text)
